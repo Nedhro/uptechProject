d3a4c7cbc8031178a8df3780c477cdcb5dc9eb39
nedhro <nedhro786>
2021-03-24 16:48:48 +0600

    project env setup

--- server.ts
+++ server.ts
@@ -1,23 +1,18 @@
-import dotenv from "dotenv";
-import express from "express";
-import path from "path";
-import bodyParser from 'body-parser';
+const dotenv = require('dotenv');
+const express = require('express');
 // initialize configuration
 dotenv.config();
 const port = process.env.SERVER_PORT;
 
 const app = express();
-// Body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
+app.get('/', (req, res) => {
+    res.send("Hello world!");
+});
 
 // start the express server
-app.listen( port, () => {
+app.listen(port, () => {
     // tslint:disable-next-line:no-console
-    console.log( `server started at http://localhost:${ port }` );
-} );
+    console.log(`server started at http://localhost:${port}`);
+});
